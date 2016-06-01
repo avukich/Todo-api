a15a94af85c1c2a982d0dd50157d324052059735
Adam Vukich <adam.vukich@intellinote.net>
2016-05-31 20:55:04 -0400

    Added PUT /todos/:id

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 
 // GET /
 app.get('/', function (req, res) {
-	res.send('Todo API Root');
+	return res.send('Todo API Root');
 });
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	return res.json(todos);
 });
 
 // GET /todos/:id
@@ -24,9 +24,9 @@ app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 	if (matchedTodo) {
-		res.json(matchedTodo);
+		return res.json(matchedTodo);
 	} else {
-		res.status(404).send();
+		return res.status(404).send();
 	}
 });
 
@@ -43,7 +43,7 @@ app.post('/todos', function (req, res) {
 	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
-	res.json(body);
+	return res.json(body);
 });
 
 // DELETE /todos/:id
@@ -52,11 +52,40 @@ app.delete('/todos/:id', function (req, res) {
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (!matchedTodo) {
-		res.status(404).send({"error": "no todo found with that id"});
+		return res.status(404).send({"error": "no todo found with that id"});
 	} else {
 		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
+		return res.json(matchedTodo);
 	}
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send({"error": "no todo found with that id"});
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && 
+		_.isString(body.description) &&
+		body.description.trim().length > 0) {
+		validAttributes.description = body.description.trim();
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
