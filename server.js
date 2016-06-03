145df6bcf9972a34c5cbd5594a8834747ca3eda7
Adam Vukich <adam.vukich@intellinote.net>
2016-06-03 11:27:27 -0400

    Added POST /users/login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -125,7 +126,31 @@ app.post('/users', function (req, res) {
 	});
 });
 
-db.sequelize.sync({force: true}).then(function () {
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	if (typeof body.email !== 'string' || 
+		typeof body.password !== 'string') {
+		return res.status(400).send();
+	} 
+
+	db.user.findOne({
+		where: {
+			email: body.email
+		}
+	}).then(function (user) {
+		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+			return res.status(401).send();
+		}
+
+		return res.json(user.toPublicJSON());
+	}, function (error) {
+		res.status(500).send();
+	});
+});
+
+db.sequelize.sync().then(function () {
 	app.listen(PORT, function () {
 		console.log('Express listening on port ' + PORT + '!');
 	});
